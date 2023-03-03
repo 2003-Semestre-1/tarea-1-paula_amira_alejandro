ae577ab86b99f8c8dcc3bd9a4bd6ee94ae955c90
alejandrocastroarayaitcr <90380164+alecastroitcr@users.noreply.github.com>
2023-03-03 07:57:45 -0600

    atributos de file reader agregados

--- src/Model/FileManager.java
+++ src/Model/FileManager.java
@@ -5,10 +5,67 @@
  */
 package Model;
 
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  *
  * @author aleja
  */
 public class FileManager {
+    
+    BufferedReader fileReader;
+    String filePath;
+    ArrayList<MemoryRegister> instructions;
+    HashMap<String,Integer> instructionHashMap;
+    HashMap<String,Integer> registerHashMap;
+
+    public FileManager(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public BufferedReader getFileReader() {
+        return fileReader;
+    }
+
+    public void setFileReader(BufferedReader fileReader) {
+        this.fileReader = fileReader;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public HashMap<String, Integer> getInstructionHashMap() {
+        return instructionHashMap;
+    }
+
+    public void setInstructionHashMap(HashMap<String, Integer> instructionHashMap) {
+        this.instructionHashMap = instructionHashMap;
+    }
+
+    public HashMap<String, Integer> getRegisterHashMap() {
+        return registerHashMap;
+    }
+
+    public void setRegisterHashMap(HashMap<String, Integer> registerHashMap) {
+        this.registerHashMap = registerHashMap;
+    }
+
+    public ArrayList<MemoryRegister> getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(ArrayList<MemoryRegister> instructions) {
+        this.instructions = instructions;
+    }
+    
+    
+    
     
 }
